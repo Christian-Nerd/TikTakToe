d4185a61944c45e478dcf6505215669dbc9efaf7
georgedavis <trey@saviortek.com>
2021-08-02 23:07:37 -0400

    V.03 Doesn't check for winner properly, but am getting cloer to V1.....

--- src/main/java/TikTakToe.java
+++ src/main/java/TikTakToe.java
@@ -1,26 +1,26 @@
 import java.util.*;
-import java.util.concurrent.TimeUnit;
 
 public class TikTakToe
 {
     static ArrayList<Integer> playerPositions = new ArrayList<Integer>();
     static ArrayList<Integer> player2Positions = new ArrayList<Integer>();
     static ArrayList<Integer> cpuPositions = new ArrayList<Integer>();
+    static String result = checkWinner();
     public static void main(String[] args)
     {
         char[][] gameBoard =
-                       {{' ', '|', ' ', '|', ' '},
+                {{' ', '|', ' ', '|', ' '},
                         {'-', '+', '-', '+', '-'},
                         {' ', '|', ' ', '|', ' '},
                         {'-', '+', '-', '+', '-'},
                         {' ', '|', ' ', '|', ' '}};
         game:
-        while(true)
+        while (true)
         {
             Scanner scan = new Scanner(System.in);
-            System.out.println("User to play tik take toe do you want to play aganist cpu or.... another player");
+            System.out.println("User to play tiktaktoe do you want to play aganist cpu or.... another player?");
             String choice = scan.nextLine().toLowerCase();
-            while(choice.equals("player") || choice.equals("another player") || choice.equals("anotherplayer"))
+            while (choice.equals("player") || choice.equals("another player") || choice.equals("anotherplayer"))
             {
                 System.out.println("Enter your placement (1-9) Player 1:");
                 int playerPos = scan.nextInt();
@@ -34,108 +34,128 @@ public class TikTakToe
                     System.out.println("Invalid Position input a playerPosition between 1-9");
                     playerPos = scan.nextInt();
                 }
-
                 placePiece(gameBoard, playerPos, "player");
-                Random rand = new Random();
-                int player2Pos = scan.nextInt();
+                printGameBoard(gameBoard);
+                result = checkWinner();
+                if(result.equals("") != true)
+                {
+                    System.out.println(result);
+                    System.out.println("");
+                    System.out.println("Want to play again? Y/N");
+                    choice = scan.nextLine().toLowerCase();
+                    boolean choseToPlay = false;
+                    choseToPlay = choice == "y" || choice == "yes" ? true : false;
+                    if (choseToPlay)
+                    {
+                        continue game;
+                    } else
+                    {
+                        break game;
+                    }
+                }
                 System.out.println("Enter your placement (1-9) Player 2:");
+                int player2Pos = scan.nextInt();
                 while (playerPositions.contains(player2Positions) || cpuPositions.contains(playerPositions))
                 {
                     System.out.println("Position taken! Enter a correct Position");
                 }
                 placePiece(gameBoard, player2Pos, "player2");
                 printGameBoard(gameBoard);
-                String result = checkWinner();
-                System.out.println(result);
-                System.out.println("Want to play again? Y/N");
-                choice = scan.nextLine().toLowerCase();
-                boolean choseToPlay = false;
-                choseToPlay = choice == "y" || choice == "yes" ? true : false;
-                if (choseToPlay)
+           if(result.equals("") != true)
+           {
+               System.out.println(result);
+               System.out.println("");
+               System.out.println("Want to play again? Y/N");
+               choice = scan.nextLine().toLowerCase();
+               boolean choseToPlay = false;
+               choseToPlay = choice == "y" || choice == "yes" ? true : false;
+               if (choseToPlay)
+               {
+                   continue game;
+               } else
+               {
+                   break game;
+               }
+           }
+       }
+
+
+            while (choice.equals("cpu"))
+            {
+                System.out.println("Enter your placement (1-9):");
+                int playerPos = scan.nextInt();
+                while (playerPositions.contains(playerPos) || cpuPositions.contains(playerPositions) || player2Positions.contains(playerPositions) || playerPositions.contains(player2Positions))
                 {
-                    continue game;
-                } else
+                    System.out.println("Position taken! Enter a correct Position");
+                    playerPos = scan.nextInt();
+                }
+                if (playerPos > 9 || playerPos < 1)
                 {
-                    break game;
+                    System.out.println("Invalid Position input a playerPosition between 1-9");
+                    playerPos = scan.nextInt();
+                }
+
+                placePiece(gameBoard, playerPos, "player");
+                printGameBoard(gameBoard);
+                Random rand = new Random();
+                int cpuPos = rand.nextInt(9) + 1;
+                while (playerPositions.contains(cpuPos) || cpuPositions.contains(cpuPositions))
+                {
+                    System.out.println("Position taken! Enter a correct Position");
+                    cpuPos = rand.nextInt(9) + 1;
+                }
+                placePiece(gameBoard, cpuPos, "cpu");
+                printGameBoard(gameBoard);
+                if(result.equals("") != true)
+                {
+                    System.out.println("......");
+                    System.out.println();
+                    System.out.println("Want to play again? Y/N");
+                    choice = scan.nextLine().toLowerCase();
+                    boolean choseToPlay = false;
+                    choseToPlay = choice == "y" || choice == "yes" ? true : false;
+                    if (choseToPlay)
+                    {
+                        continue game;
+                    } else
+                    {
+                        break game;
+                    }
                 }
             }
-
-          while(choice.equals("cpu"))
-          {
-              System.out.println("Enter your placement (1-9):");
-              int playerPos = scan.nextInt();
-              while (playerPositions.contains(playerPos) || cpuPositions.contains(playerPositions) || player2Positions.contains(playerPositions) || playerPositions.contains(player2Positions))
-              {
-                  System.out.println("Position taken! Enter a correct Position");
-                  playerPos = scan.nextInt();
-              }
-              if (playerPos > 9 || playerPos < 1)
-              {
-                  System.out.println("Invalid Position input a playerPosition between 1-9");
-                  playerPos = scan.nextInt();
-              }
-
-              placePiece(gameBoard, playerPos, "player");
-              Random rand = new Random();
-              int cpuPos = rand.nextInt(9) + 1;
-              while (playerPositions.contains(cpuPos) || cpuPositions.contains(cpuPositions))
-              {
-                  System.out.println("Position taken! Enter a correct Position");
-                  cpuPos = rand.nextInt(9) + 1;
-              }
-              placePiece(gameBoard, cpuPos, "cpu");
-              printGameBoard(gameBoard);
-              String result = checkWinner();
-              System.out.println(result);
-              System.out.println("Want to play again? Y/N");
-              choice = scan.nextLine().toLowerCase();
-              boolean choseToPlay = false;
-              choseToPlay = choice == "y" || choice == "yes" ? true : false;
-              if (choseToPlay)
-              {
-                  continue game;
-              }
-              else
-              {
-                  break game;
-              }
-          }
         }
     }
 
     public static void printGameBoard(char[][] gameBoard)
     {
-            for(char[] row : gameBoard)
+        for (char[] row : gameBoard)
+        {
+            for (char c : row)
             {
-                for (char c : row)
-                {
-                    System.out.print(c);
-                }
-                System.out.println();
+                System.out.print(c);
             }
+            System.out.println();
+        }
 
     }
 
     public static void placePiece(char[][] gameBoard, int playerPos, String user)
     {
         char symbol = ' ';
-        if(user.equals("player"))
+        if (user.equals("player"))
         {
             symbol = 'X';
             playerPositions.add(playerPos);
-        }
-
-        else if(user.equals("cpu"))
+        } else if (user.equals("cpu"))
         {
             symbol = '0';
             cpuPositions.add(playerPos);
-        }
-        else if(user.equals("player2"))
+        } else if (user.equals("player2"))
         {
             symbol = '0';
             player2Positions.add(playerPos);
         }
-        switch(playerPos)
+        switch (playerPos)
         {
             case 1:
                 gameBoard[0][0] = symbol;
@@ -190,20 +210,19 @@ public class TikTakToe
         winning.add(cross1);
         winning.add(cross2);
 
-        for(List l : winning)
+        for (List l : winning)
         {
-           if(playerPositions.containsAll(l))
-           {
-               return "Congratulations you won at tik tak toe!" + " Wow You must be bigbrain to get this!";
-           }
-           else if(playerPositions.size() + cpuPositions.size() == 9)
-           {
-               return "TIE! NO ONE WINS!!!";
-           }
+            if (playerPositions.containsAll(l))
+            {
+                return "Congratulations you won at tik tak toe!" + " Wow You must be bigbrain to get this!";
+            } else if (playerPositions.size() + cpuPositions.size() == 9)
+            {
+                return "TIE! NO ONE WINS!!!";
+            }
         }
         return "";
     }
-
 }
+
 
 
